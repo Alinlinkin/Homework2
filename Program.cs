5710c54f5c746142b929038fde6ab5bec9ce7744
alilinkin <sail94@mail.ru>
2022-09-16 22:16:42 +0500

    2 число из трехзначного

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿void SecondNumber(int x)
+{
+    if (x > 99 && x < 1000)
+    {
+        int result = x / 10 % 10;
+        Console.WriteLine($"Вторая цифра числа {x} = {result}");
+    }
+    else if (x > -1000 && x < -99)
+    {
+        x = x * -1;
+        int result = x / 10 % 10;
+        Console.WriteLine($"Вторая цифра числа {-x} = {result}");
+    }
+    else { Console.WriteLine("Число не трехзначное, попробуйте снова :)"); }
+}
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+SecondNumber(number);
